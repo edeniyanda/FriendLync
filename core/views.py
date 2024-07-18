49680a2f266c7874827a9d3c24746cb421ba0bec
Eden Iyanda <edeniyanda@gmail.com>
2024-07-18 18:55:27 +0100

    Added message tag

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +10,23 @@ def index(request):
 def signup(request):
 
     if request.method == "POST":
-        pass
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password1"]
+        confirmpassword = request.POST["password2"]
+
+        if password == confirmpassword:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email Already Taken")
+                return redirect("signup")
+            elif User.objects.filter(username=username).exists():
+                messages.info.exists(request, "Username Already Taken")
+                return redirect("signup")
+            else:
+                user = User.objects.create_user(username=username, email=email, password=confirmpassword)
+                user.save()
+        else:
+            messages.info(request, "Password doesn't Match")
+            return redirect("signup")
     else:
         return render(request, "signup.html")
