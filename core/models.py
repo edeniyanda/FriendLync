d27ab7a6d7e5aece0981e6a0291f5bc8cb4becd7
Eden Iyanda <edeniyanda@gmail.com>
2024-07-20 15:29:08 +0100

    Update: Created Post model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
+import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
+from datetime import datetime
+
 
 User = get_user_model()
 
@@ -13,3 +16,14 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="post_images")
+    caption = models.TextField()
+    created_at = models.DateField(default=datetime.now())
+    no_of_likes = models.IntegerField(default=0 )
+
+    def __str__(self) -> str:
+        return self.user
