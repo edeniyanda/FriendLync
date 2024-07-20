a812eda2ef9becf81957a6bdd5264df7ae7c0b55
Eden Iyanda <edeniyanda@gmail.com>
2024-07-20 14:15:58 +0100

    Update: Loaded user data to the settings form

--- core/views.py
+++ core/views.py
@@ -64,4 +64,7 @@ def logout(request):
 
 @login_required(login_url="signin")
 def settings(request):
-    return render(request, "setting.html")
+    user_profile = Profile.objects.get(user=request.user)
+    return render(request, "setting.html", {
+        "user_profile" : user_profile
+    })
