587bc3e8114f251bd3cb4c1ab3c3779b176a6fcd
Eden Iyanda <edeniyanda@gmail.com>
2024-07-20 15:33:34 +0100

    Update: Made migration for the Post model

--- core/models.py
+++ core/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     user = models.CharField(max_length=100)
     image = models.ImageField(upload_to="post_images")
     caption = models.TextField()
-    created_at = models.DateField(default=datetime.now())
+    created_at = models.DateField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0 )
 
     def __str__(self) -> str:
